62cb84ded96cc082a884d43321650a3798b26efd
ahad raza <98664240+ahadraza0@users.noreply.github.com>
2022-02-06 16:31:45 +0500

    Add files via upload

---
+++ JAVA LECTURE 7.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num1=533333;
+        int num2=46;
+        int num3=43333;
+        if((num1>num2)&&(num1>num3))
+        {
+            System.out.println("num1 is greater then other number");
+        }
+        else if((num2>num1)&&(num2>num3))
+        {
+            System.out.println("num2 is greater then other number");
+        }
+        else
+        {
+            System.out.println("num3 is greater then other number");
+        }
+    }
+}
+
---
+++ JAVA LECTURE 8.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num1=23;
+        int num2=22;
+        if(num1%num2==0)
+        {
+            System.out.println("yuor number is even");
+        }
+        else
+        {
+            System.out.println("your number is odd");
+        }
+    }
+}
+
